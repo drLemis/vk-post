8bdfea87193ca62fb6046ae4566b8101f6027cf0
Lemis <remminarsettleher@mail.ru>
2020-06-04 23:20:37 +0300

    Hotfix

--- js/web.js
+++ js/web.js
@@ -1,9 +1,11 @@
+var code = 0;
 var token = 0;
 var version = 5.57;
 
 function checkURIforRedirect() {
 	if (location.hash != "") {
 		token = location.hash.split("=")[1].split("&")[0];
+		code = location.search.split("=")[1];
 
 		getAccessToken();
 
@@ -12,13 +14,6 @@ function checkURIforRedirect() {
 			document.getElementById("mainInterface").removeAttribute("hidden");
 		}
 	}
-}
-
-function getToken() {
-	window.location.replace(
-		"https://oauth.vk.com/authorize?client_id=7498813&display=popup&redirect_uri=https://drlemis.github.io/vk-post/&scope=wall,groups&response_type=token&v=" +
-			version
-	);
 }
 
 function getCode() {
@@ -33,7 +28,7 @@ function getAccessToken() {
 	$.ajax({
 		url:
 			"https://oauth.vk.com/access_token?client_id=7498813&client_secret=5bgyJMq0GzysFMSlLGW3&redirect_uri=https://drlemis.github.io/vk-post/&code=" +
-			token,
+			code,
 		method: "GET",
 		dataType: "JSONP",
 		crossDomain: true,
