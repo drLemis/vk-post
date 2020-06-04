3f9708996380513d63b98c83eeb69d289653589c
Lemis <remminarsettleher@mail.ru>
2020-06-04 22:41:35 +0300

    Login rework

--- js/web.js
+++ js/web.js
@@ -1,34 +1,28 @@
 var token = 0;
-var ID = 0;
 var version = 5.57;
 
 function checkURIforRedirect() {
 	if (location.hash != "") {
-		var resultArray = location.hash.split("=");
-		token = resultArray[resultArray.length - 1];
-		ID = resultArray[1].split("_")[2];
-		postWall();
+		token = location.hash.split("=")[0].split("&")[0];
 	}
 }
 
-function getToken(groupID) {
+function getToken() {
 	window.location.replace(
-		"https://oauth.vk.com/authorize?client_id=7498813&group_ids=" +
-			groupID +
-			"&display=page&redirect_uri=" +
-			location +
-			"&scope=manage,wall&response_type=token&v=" +
+		"https://oauth.vk.com/authorize?client_id=7498813&display=popup&redirect_uri=https://drlemis.github.io/vk-post/&scope=wall&response_type=token&v=" +
 			version
 	);
 }
 
-function postWall() {
+function postWall(groupID, text) {
 	console.log("WALLPOST" + ID);
 	$.ajax({
 		url:
-			"https://api.vk.com/method/wall.post?owner_id=" +
-			ID +
-			"&message='TEST EXAMPLE'&access_token=" +
+			"https://api.vk.com/method/wall.post?owner_id=-" +
+			groupID +
+			"&message='" +
+			text +
+			"'&access_token=" +
 			token +
 			"&v=" +
 			version,
