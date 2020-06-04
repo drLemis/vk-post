f62048bc41c498fae65656e13ea203e979e94861
Lemis <remminarsettleher@mail.ru>
2020-06-04 22:51:09 +0300

    Hotfix

--- js/web.js
+++ js/web.js
@@ -1,5 +1,4 @@
 var token = 0;
-var version = 5.57;
 
 function checkURIforRedirect() {
 	if (location.hash != "") {
@@ -14,23 +13,21 @@ function checkURIforRedirect() {
 
 function getToken() {
 	window.location.replace(
-		"https://oauth.vk.com/authorize?client_id=7498813&display=popup&redirect_uri=https://drlemis.github.io/vk-post/&scope=wall&response_type=token&v=" +
-			version
+		"https://oauth.vk.com/authorize?client_id=7498813&display=popup&redirect_uri=https://drlemis.github.io/vk-post/&scope=wall&response_type=token"
 	);
 }
 
 function postWall(groupID, text) {
 	console.log("WALLPOST:" + groupID + ":" + text);
+	console.log("TOKEN:" + token);
 	$.ajax({
 		url:
 			"https://api.vk.com/method/wall.post?owner_id=-" +
 			groupID +
-			"&message='" +
+			"&message=" +
 			text +
-			"'&access_token=" +
-			token +
-			"&v=" +
-			version,
+			"&access_token=" +
+			token,
 		method: "GET",
 		dataType: "JSONP",
 		crossDomain: true,
