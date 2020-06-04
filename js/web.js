5372f24ce7ffec78dee6c6f99491d97368c57632
Lemis <remminarsettleher@mail.ru>
2020-06-04 21:23:52 +0300

    Redirect rework

--- js/web.js
+++ js/web.js
@@ -8,36 +8,43 @@ function getJSONToFunc(url, func) {
 	});
 }
 
+function checkURIforRedirect() {
+	if (location.hash != "") {
+		console.log("REDIRECTED");
+		token = location.hash.split("=")[1];
+		console.log(token);
+		document.getElementById("groupID").textContent = "REDIRECTED";
+		login();
+	}
+}
 
 function getToken(groupID) {
 	console.log("GET TOKEN");
-	$.ajax(
-		{
-			url: "https://oauth.vk.com/authorize?client_id=7498813&group_ids="+groupID+"&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=users&response_type=token&v=5.107",
-			method: "GET",
-			dataType: "text",
-			crossDomain: true,
-			success: function (data) {
-				console.log(data);
-				document.getElementById('groupID').textContent = "PARTIAL SUCCESS!"
-				login();
-			}
-		}
-	)
+
+	window.location.replace(
+		"https://oauth.vk.com/authorize?client_id=7498813&group_ids=" +
+			groupID +
+			"&display=page&redirect_uri=" +
+			location +
+			"&scope=manage&response_type=token&v=5.107"
+	);
 }
 
 function login() {
 	console.log("LOGIN");
-	$.ajax(
-		{
-			url: "https://api.vk.com/method/users.get?PARAMETERS&access_token="+token+"&v=5.107",
-			method: "GET",
-			dataType: "JSONP",
-			crossDomain: true,
-			success: function (data) {
-				console.log(data);
-				document.getElementById('groupID').textContent = "SUCCESS!"
-			}
-		}
-	)
+	$.ajax({
+		url:
+			"https://api.vk.com/method/users.get?PARAMETERS&access_token=" +
+			token +
+			"&v=5.107",
+		method: "GET",
+		dataType: "JSONP",
+		crossDomain: true,
+		success: function (data) {
+			console.log(data);
+			document.getElementById("groupID").textContent = "SUCCESS!";
+		},
+	});
 }
+
+checkURIforRedirect();
