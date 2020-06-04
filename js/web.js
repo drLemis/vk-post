11c2664cfefc87af601c9d2b0510e3d983a33b4c
Lemis <remminarsettleher@mail.ru>
2020-06-04 23:17:57 +0300

    Rework for standalone

--- js/web.js
+++ js/web.js
@@ -5,6 +5,8 @@ function checkURIforRedirect() {
 	if (location.hash != "") {
 		token = location.hash.split("=")[1].split("&")[0];
 
+		getAccessToken();
+
 		if (token != "") {
 			document.getElementById("loginButton").setAttribute("hidden", "true");
 			document.getElementById("mainInterface").removeAttribute("hidden");
@@ -14,9 +16,33 @@ function checkURIforRedirect() {
 
 function getToken() {
 	window.location.replace(
-		"https://oauth.vk.com/authorize?client_id=7498813&display=popup&redirect_uri=https://drlemis.github.io/vk-post/&scope=wall&response_type=token&v=" +
+		"https://oauth.vk.com/authorize?client_id=7498813&display=popup&redirect_uri=https://drlemis.github.io/vk-post/&scope=wall,groups&response_type=token&v=" +
 			version
 	);
+}
+
+function getCode() {
+	window.location.replace(
+		"https://oauth.vk.com/authorize?client_id=7498813&display=popup&redirect_uri=https://drlemis.github.io/vk-post/&scope=wall,groups&response_type=code&v=" +
+			version
+	);
+}
+
+function getAccessToken() {
+	//
+	$.ajax({
+		url:
+			"https://oauth.vk.com/access_token?client_id=7498813&client_secret=5bgyJMq0GzysFMSlLGW3&redirect_uri=https://drlemis.github.io/vk-post/&code=" +
+			token,
+		method: "GET",
+		dataType: "JSONP",
+		crossDomain: true,
+		success: function (data) {
+			console.log("ACCESS");
+			console.log(data);
+			token = data.access_token;
+		},
+	});
 }
 
 function postWall(groupID, text) {
