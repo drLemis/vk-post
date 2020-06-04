2347b90c35b2d12264ae9e803ebaf65bef8b2970
Lemis <remminarsettleher@mail.ru>
2020-06-04 22:13:39 +0300

    Wall post update

--- js/web.js
+++ js/web.js
@@ -1,4 +1,5 @@
 var token = 0;
+var ID = 0;
 var version = 5.57;
 
 function getJSONToFunc(url, func) {
@@ -13,6 +14,7 @@ function checkURIforRedirect() {
 	if (location.hash != "") {
 		var resultArray = location.hash.split("=");
 		token = resultArray[resultArray.length - 1];
+		ID = resultArray[1].split("_")[2];
 		login();
 	}
 }
@@ -47,10 +49,12 @@ function login() {
 }
 
 function postWall() {
-	console.log("LOGIN");
+	console.log("WALLPOST" + id);
 	$.ajax({
 		url:
-			"https://api.vk.com/method/wall.post?owner_id=-1&message='TEST EXAMPLE'&access_token=" +
+			"https://api.vk.com/method/wall.post?owner_id=" +
+			ID +
+			"&message='TEST EXAMPLE'&access_token=" +
 			token +
 			"&v=" +
 			version,
