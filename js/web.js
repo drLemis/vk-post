9d8a2d79a28f2ffe82f169c1c65c9058d4ddddb4
Lemis <remminarsettleher@mail.ru>
2020-06-05 02:32:59 +0300

    spray'n'pray

--- js/web.js
+++ js/web.js
@@ -2,6 +2,8 @@ var version = 5.73;
 
 var pictures = {};
 
+var timerPostPictures;
+
 VK.init(
 	function () {
 		console.log("VK API success!");
@@ -19,16 +21,42 @@ function getAlbumToPictures(groupID, albumID) {
 		{ owner_id: groupID, album_id: albumID, rev: 0, v: version },
 		function (data) {
 			console.log(data);
+			pictures = data.response.items;
 		}
 	);
 }
 
-function postWall(groupID, text) {
-	VK.api(
-		"wall.post",
-		{ owner_id: groupID, message: text, from_group: 1, v: version },
-		function (data) {
-			console.log(data);
-		}
-	);
+function postPicturesToWall(groupID) {
+	var timerOffset = 500;
+
+	timerPostPictures = setInterval(() => postWall(groupID), timerOffset);
+
+	setTimeout(() => {
+		clearInterval(timerPostPictures);
+		postWall();
+	}, timerOffset * 10);
+
+	for (let index = 0; index < pictures.length; index++) {
+		const element = array[index];
+	}
+}
+
+function postWall(groupID, photoID) {
+	if (pictures.length > 0) {
+		VK.api(
+			"wall.post",
+			{
+				owner_id: groupID,
+				message: "",
+				attachment: "photo"+pictures.pop().id,
+				from_group: 1,
+				v: version,
+			},
+			function (data) {
+				console.log(data);
+			}
+		);
+	} else {
+		clearInterval(timerPostPictures);
+	}
 }
