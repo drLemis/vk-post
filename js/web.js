727e31fa9a86a67465b11fbb20a28f76ae60ea20
Lemis <remminarsettleher@mail.ru>
2020-06-05 17:45:37 +0300

    Basic framework for delayed posting

--- js/web.js
+++ js/web.js
@@ -1,5 +1,23 @@
 var version = 5.73;
+
+var groupID;
+var albumID;
+var wallID;
+var datePost;
+var timePost;
+var errorPost;
+
 var pictures = {};
+var times = {};
+
+function processInput() {
+	groupID = document.getElementById("groupID").value;
+	albumID = document.getElementById("albumID").value;
+	wallID = document.getElementById("wallID").value;
+	datePost = document.getElementById("datePost").value;
+	timePost = document.getElementById("timePost").value.split(",");
+	errorPost = document.getElementById("errorPost").value * 60;
+}
 
 VK.init(
 	function () {
@@ -11,32 +29,67 @@ VK.init(
 	version
 );
 
-function getAlbumToPictures(groupID, albumID, wallID) {
+function getAlbumToPictures() {
 	VK.api(
 		"photos.get",
 		{ owner_id: groupID, album_id: albumID, rev: 0, v: version },
 		function (data) {
 			pictures = data.response.items;
-			postWall(groupID, wallID);
+			makeTimes();
+			postWall();
 		}
 	);
 }
 
-function postWall(groupID, wallID) {
+function makeTimes() {
+	var tempTimes = {};
+	var times = {};
+
+	var unixtimeStart = parseInt(
+		(new Date(datePost).getTime() / 1000).toFixed(0)
+	);
+
+	var subindex = 0;
+
+	for (let index = 0; index < pictures.Length; index++) {
+		times.Add(
+			unixtimeStart +
+				timePost[subindex] * 360 +
+				getRandomInt(-errorPost, errorPost)
+		);
+
+		if (subindex++ >= timePost.Length) {
+			subindex = 0;
+			unixtimeStart += 86400; //new day
+		}
+	}
+}
+
+function postWall() {
 	if (pictures.length > 0) {
 		var picID = "photo" + groupID + "_" + pictures.pop().id;
-		console.log(picID);
+
+		var timeToPost;
 		VK.api(
 			"wall.post",
 			{
 				owner_id: wallID,
 				attachment: picID,
 				from_group: 1,
+				publish_date: times.pop(),
 				v: version,
 			},
 			function (data) {
-				postWall(groupID, wallID);
+				postWall();
 			}
 		);
 	}
+}
+
+// ========
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
 }
